60921df558e7f6b83ceff6034c622947f05f22bb
VladSkopenko <skopirka2k17@gmail.com>
2024-08-25 23:55:09 +0300

    add docker

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ import asyncio
 import os
 import sys
 
-sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), "")))
 from aiogram import Bot
 from aiogram import Dispatcher
 from aiogram import types
